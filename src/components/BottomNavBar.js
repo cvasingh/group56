59251244f1313e9a28192ca9d72123b970e78dcc
Shiva Singh <shivasingh.200087@gmail.com>
2023-05-30 15:20:51 +0530

    done

--- src/components/BottomNavBar.js
+++ src/components/BottomNavBar.js
@@ -9,10 +9,14 @@ export default function BottomNavBar() {
             {[
                 { name: 'Home', link: '/' },
                 { name: 'Camera', link: '/scan' },
-                { name: 'Setting', link: '/upload' }
+                // { name: 'Setting', link: '/upload' }
             ].map((ele, i) =>
                 <NavBottom key={i} active={active === i} setActive={() => setActive(i)} ele={ele} />
             )}
+            <a href="http://18.119.29.1:3000" className={`text-xs whitespace-nowrap rounded-3xl py-2 px-6 duration-300 ease-linear
+            ${active ? 'font-bold text-[#202020] bg-[#02e18b]' : 'font-medium text-[#202020]'}`}>
+                Upload
+            </a>
         </div>
     )
 }
